cc2d44c2c7a11921b90cfef264a5a2ac8bae139a
황희원 <heewon7318@naver.com>
2022-07-25 21:48:11 +0900

    [S07P12D209-111] setting - vue initial setting

---
+++ backend/src/main/java/com/d209/mungtopia/controller/ManageApplicantController.java
@@ -0,0 +1,9 @@
+package com.d209.mungtopia.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/manage/applicants")
+public class ManageApplicantController {
+}

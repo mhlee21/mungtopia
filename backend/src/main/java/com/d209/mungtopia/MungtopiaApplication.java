c05c2211eb66cc6737b3b163761a344e29363ad6
mhlee <mihyun0921@gmail.com>
2022-07-25 13:16:38 +0900

    [S07P12D209-112] springboot initial setting

---
+++ backend/src/main/java/com/d209/mungtopia/MungtopiaApplication.java
@@ -0,0 +1,13 @@
+package com.d209.mungtopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungtopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungtopiaApplication.class, args);
+	}
+
+}

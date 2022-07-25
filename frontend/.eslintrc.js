24dde290efe8b5b93503c412b05059b94c98ce86
황희원 <huiwon1488@gmail.com>
2022-07-25 15:59:22 +0900

    [S07P12D209-111] setting - initial setting
    !1

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,33 @@
+//.eslintrc.js
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+	},
+	rules: {
+		'vue/no-multiple-template-root': 'off',
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+	},
+};

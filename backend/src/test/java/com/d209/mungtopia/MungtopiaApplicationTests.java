0bece02a2d01b350624fcf710062cbdbb400215c
mhlee <mihyun0921@gmail.com>
2022-07-25 13:16:38 +0900

    [S07P12D209-112] springboot initial setting

---
+++ backend/src/test/java/com/d209/mungtopia/MungtopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d209.mungtopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungtopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

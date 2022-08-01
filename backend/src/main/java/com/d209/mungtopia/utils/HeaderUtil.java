11ff462c32278dc713f581843e2de2a125c43ebd
SSAFY <anwlro7958>
2022-08-01 17:51:36 +0900

    [S07P12D209-240] feature - 소셜로그인 주석 추가 , Swagger 설정 추가

--- backend/src/main/java/com/d209/mungtopia/utils/HeaderUtil.java
+++ backend/src/main/java/com/d209/mungtopia/utils/HeaderUtil.java
@@ -2,6 +2,13 @@ package com.d209.mungtopia.utils;
 
 import javax.servlet.http.HttpServletRequest;
 
+/*
+토큰이
+{
+	Authorization : Bearer +토큰
+}
+이러한 형태로 들어올 때 확인하는 Util
+*/
 public class HeaderUtil {
 
     private final static String HEADER_AUTHORIZATION = "Authorization";

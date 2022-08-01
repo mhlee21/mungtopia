0390f69ba374ad9cae49dc999a220ba75dc2b7be
mhlee <mihyun0921@gmail.com>
2022-08-01 21:08:05 +0900

    [S07P12D209-280] test - í테스트를 위한 API 추가

---
+++ backend/src/main/java/com/d209/mungtopia/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.d209.mungtopia.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/")
+    private ResponseEntity<LocalDate> test() {
+        LocalDate now = LocalDate.now();
+        return new ResponseEntity<LocalDate>(now, HttpStatus.OK);
+    }
+}

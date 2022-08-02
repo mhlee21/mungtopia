fc70071c9bfbb32845381d9fe80582b5a88a2162
heewon7318 <huiwon1488@gmail.com>
2022-08-02 13:51:07 +0900

    [S07P12D209-230] feature - 입양관리-vuex 연동-메인-희원

---
+++ frontend/src/components/adopt/main/ApplicantMainComponent.vue
@@ -0,0 +1,50 @@
+<template>
+	<div
+		style="display: flex; align-items: center"
+		@click="clickApplication(application.application_id)"
+	>
+		<img
+			:src="application.dog_img"
+			alt="dog image"
+			class="application-dog-img"
+		/>
+		<div class="application-dog-info">
+			<h4 style="margin: 20% 10%">
+				{{ application.dog_name }}
+			</h4>
+		</div>
+	</div>
+</template>
+
+<script>
+import router from '@/router';
+
+export default {
+	props: { application: Object },
+	setup() {
+		const clickApplication = applicationId => {
+			router.push({
+				name: 'applicantDetail',
+				params: { applicationId: applicationId },
+			});
+		};
+		return { clickApplication };
+	},
+};
+</script>
+
+<style scoped>
+.application-dog-img {
+	width: 35vw;
+	height: 35vw;
+	border-radius: 1rem;
+	box-shadow: 1px 4px 3px rgba(0, 0, 0, 0.2);
+}
+.application-dog-info {
+	background-color: white;
+	width: 30vw;
+	height: 15vh;
+	border-radius: 0 1rem 1rem 0;
+	box-shadow: 1px 4px 3px rgba(0, 0, 0, 0.2);
+}
+</style>
--- frontend/src/views/adopt/ApplicantDetailView.vue
+++ frontend/src/views/adopt/ApplicantDetailView.vue
@@ -1,18 +1,25 @@
 <template>
 	<div>
-		<button>back</button>
+		<button @click="goAdoptMain">back</button>
 		<ApplicantDetailComponent></ApplicantDetailComponent>
 		<AdoptDetailProcedure></AdoptDetailProcedure>
 	</div>
 </template>
 
 <script>
+import router from '@/router';
 import ApplicantDetailComponent from '@/components/adopt/detail/ApplicantDetailComponent';
 import AdoptDetailProcedure from '@/components/adopt/detail/AdoptDetailProcedure';
 
 export default {
 	setup() {
-		return {};
+		const goAdoptMain = () => {
+			router.push({
+				name: 'adopt',
+				// params: { appicantId },
+			});
+		};
+		return { goAdoptMain };
 	},
 	components: { ApplicantDetailComponent, AdoptDetailProcedure },
 };

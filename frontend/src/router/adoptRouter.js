e7e22ecf6104a968098a8e7df6088db9ef97799c
heewon7318 <huiwon1488@gmail.com>
2022-08-02 15:53:58 +0900

    [S07P12D209-298] fix - FE router 수정 및 폴더 구조 수정

---
+++ frontend/src/router/adoptRouter.js
@@ -0,0 +1,33 @@
+import AdoptMainView from '@/views/adopt/AdoptMainView';
+import ApplicantDetailView from '@/views/adopt/ApplicantDetailView';
+import ProtectorDetailView from '@/views/adopt/ProtectorDetailView';
+import ChatView from '@/views/adopt/ChatView';
+import MeetingView from '@/views/adopt/MeetingView';
+
+export default [
+	{
+		path: '/adopt/:appicantId',
+		name: 'adopt',
+		component: AdoptMainView,
+	},
+	{
+		path: '/adopt/applicant/:applicationId/detail',
+		name: 'applicantDetail',
+		component: ApplicantDetailView,
+	},
+	{
+		path: '/adopt/protector/:boardId/detail',
+		name: 'protectorDetail',
+		component: ProtectorDetailView,
+	},
+	{
+		path: '/adopt/chat/:chatRoomId',
+		name: 'chat',
+		component: ChatView,
+	},
+	{
+		path: '/adopt/meeting/:meetingRoomId',
+		name: 'meeting',
+		component: MeetingView,
+	},
+];

b5dd04eda8e8507a9a8575b11c8f21c6ac31c655
anwlro7958 <anwlro7958@gmail.com>
2022-08-03 22:45:43 +0900

    [S07P12D209-263] feature - Service, Repository 구현

--- backend/src/main/java/com/d209/mungtopia/service/ManageProtectorServiceImpl.java
+++ backend/src/main/java/com/d209/mungtopia/service/ManageProtectorServiceImpl.java
@@ -1,2 +1,14 @@
-package com.d209.mungtopia.service;public class ManageProtectorServiceImpl {
+package com.d209.mungtopia.service;
+
+import com.d209.mungtopia.repository.ManageProtectorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// BL
+@Service
+@RequiredArgsConstructor
+public class ManageProtectorServiceImpl implements ManageProtectorService{
+
+    private final ManageProtectorRepository manageProtectorRepository;
+
 }

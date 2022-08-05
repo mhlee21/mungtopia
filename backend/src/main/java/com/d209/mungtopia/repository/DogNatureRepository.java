e3af2678e8d471f2162cad1b954e3c894e0ceb23
mhlee <mihyun0921@gmail.com>
2022-08-05 23:09:13 +0900

    chore - repository file 생성

---
+++ backend/src/main/java/com/d209/mungtopia/repository/DogNatureRepository.java
@@ -0,0 +1,9 @@
+package com.d209.mungtopia.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class DogNatureRepository {
+}

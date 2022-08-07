c1064e63e1cbe1b2aaf480b92cc167bff56496cf
heewon7318 <huiwon1488@gmail.com>
2022-08-08 04:28:03 +0900

    [S07P12D209-315] feature - 게시판 글 작성 페이지 마크업

--- frontend/src/components/board/create/BaseUploadImage.vue
+++ frontend/src/components/board/create/BaseUploadImage.vue
@@ -1,11 +1,22 @@
 <template>
-	<div></div>
+	<div class="image-box" v-if="applicationPageNum === 1">
+		<i class="fa-solid fa-image image-upload-icon"></i>
+		<p class="image-upload-notice">
+			사진을 업로드 해주세요. <br />최대 3장까지 가능합니다.
+		</p>
+	</div>
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 export default {
 	setup() {
-		return {};
+		const store = useStore();
+		const applicationPageNum = computed(
+			() => store.getters['board/applicationPageNum'],
+		);
+		return { applicationPageNum };
 	},
 };
 </script>

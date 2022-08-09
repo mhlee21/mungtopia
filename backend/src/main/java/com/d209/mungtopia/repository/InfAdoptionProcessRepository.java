718052813be5708d79c9e7bbcba9d96ccdb717b9
anwlro7958 <yeonjeong.lee00@gmail.com>
2022-08-09 22:01:00 +0900

    [S07P12D209-275] feature - 입양관리 공통 API 개발 완료

---
+++ backend/src/main/java/com/d209/mungtopia/repository/InfAdoptionProcessRepository.java
@@ -0,0 +1,7 @@
+package com.d209.mungtopia.repository;
+
+import com.d209.mungtopia.entity.AdoptionProcess;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InfAdoptionProcessRepository extends JpaRepository<AdoptionProcess, Long> {
+}
--- backend/src/main/java/com/d209/mungtopia/service/ManageServiceImpl.java
+++ backend/src/main/java/com/d209/mungtopia/service/ManageServiceImpl.java
@@ -5,9 +5,7 @@ import com.d209.mungtopia.dto.manage.ApplicationDto;
 import com.d209.mungtopia.dto.manage.ChatDto;
 import com.d209.mungtopia.dto.protector.ApplicantProcessRes;
 import com.d209.mungtopia.entity.*;
-import com.d209.mungtopia.repository.AdoptionProcessRepository;
-import com.d209.mungtopia.repository.AdoptionStepDateRepository;
-import com.d209.mungtopia.repository.ApplicationRepository;
+import com.d209.mungtopia.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,8 +24,8 @@ public class ManageServiceImpl implements ManageService{
 
     private final ApplicationRepository applicationRepository;
     private final AdoptionProcessRepository adoptionProcessRepository;
-    private final AdoptionStepDateRepository adoptionStepDateRepository;
-    private final IAdoptionStepDateRepository iAdoptionStepDateRepository;
+    private final InfAdoptionStepDateRepository infAdoptionStepDateRepository;
+    private final InfAdoptionProcessRepository infAdoptionProcessRepository;
 
     @Override
     public ApplicationDto getApplication(Long appId) {
@@ -68,43 +66,51 @@ public class ManageServiceImpl implements ManageService{
     @Override
     public ApplicantProcessRes getSchedule(Long adoptionProcessId) {
         ApplicantProcessRes response = new ApplicantProcessRes();
-        AdoptionProcess adoptionProcess = adoptionProcessRepository.find(adoptionProcessId);
-        int step = adoptionProcess.getStep(); // 현재 step
-        List<AdoptionStepDate> adoptionStepDateList = adoptionProcess.getAdoptionStepDateList();
-        for (AdoptionStepDate stepDate: adoptionStepDateList) {
-            if(stepDate.getStep() == step){
-                response.setStep(step);
-                response.setDate(stepDate.getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-            }
-        }
+        Optional<AdoptionProcess> adoptionProcess = infAdoptionProcessRepository.findById(adoptionProcessId);
+        if (adoptionProcess.isEmpty()) // 없으면 null 반환
+            return null;
+        // 있으면 찾아서 반환
+        int step = adoptionProcess.get().getStep(); // 현재 step
+        Optional<AdoptionStepDate> stepDate = infAdoptionStepDateRepository.findByAdoptionProcessAndStep(adoptionProcess.get(), step);
+        if (stepDate.isEmpty())
+            return null;
+        response.setStep(stepDate.get().getStep());
+        if (stepDate.get().getDate() != null) // null일 수 있음!
+            response.setDate(stepDate.get().getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        else
+            response.setDate(null);
         return response;
     }
 
     @Override
     @Transactional
     public Boolean postSchedule(Long adoptionProcessId, LocalDateTime dateTime) {
-        AdoptionProcess adoptionProcess = adoptionProcessRepository.find(adoptionProcessId);
-        int step = adoptionProcess.getStep(); // 현재 step
-        List<AdoptionStepDate> adoptionStepDateList = adoptionProcess.getAdoptionStepDateList();
-        for (AdoptionStepDate stepDate: adoptionStepDateList) {
-            if(stepDate.getStep() == step){
-                stepDate.changeDate(dateTime); // 날짜 수정
-                return true;
-            }
-        }
-        return false;
+        Optional<AdoptionProcess> adoptionProcess = infAdoptionProcessRepository.findById(adoptionProcessId);
+        if (adoptionProcess.isEmpty())
+            return false;
+        int step = adoptionProcess.get().getStep(); // 현재 step
+        Optional<AdoptionStepDate> stepDate = infAdoptionStepDateRepository.findByAdoptionProcessAndStep(adoptionProcess.get(), step);
+        if (stepDate.isEmpty())
+            return false;
+        stepDate.get().changeDate(dateTime);
+        infAdoptionStepDateRepository.save(stepDate.get());
+        return true;
     }
 
     @Override
     @Transactional
     public Boolean deleteSchedule(Long adoptionProcessId) {
-        AdoptionProcess adoptionProcess = adoptionProcessRepository.find(adoptionProcessId);
-        int step = adoptionProcess.getStep(); // 현재 step
-        List<AdoptionStepDate> adoptionStepDateList = adoptionProcess.getAdoptionStepDateList();
-        for (AdoptionStepDate stepDate: adoptionStepDateList) {
-            if(stepDate.getStep() == step){
-                stepDate.changeDate(LocalDateTime.of(0, 0, 0, 0, 0)); // null로 수정  0000-00-00 00:00:00
-                return true;
+        Optional<AdoptionProcess> adoptionProcess = infAdoptionProcessRepository.findById(adoptionProcessId);
+        if (adoptionProcess.isPresent()){
+            int step = adoptionProcess.get().getStep(); // 현재 step
+            List<AdoptionStepDate> adoptionStepDateList = adoptionProcess.get().getAdoptionStepDateList();
+            System.out.println("adoptionStepDateList.size() = " + adoptionStepDateList.size());
+            for (AdoptionStepDate stepDate: adoptionStepDateList) {
+                if(stepDate.getStep() == step){
+                    Long stepDateId = stepDate.getAdoptionStepDateId();
+                    infAdoptionStepDateRepository.delete(stepDate);
+                    return true;
+                }
             }
         }
         return false;

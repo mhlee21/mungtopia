3468e3234bf4a2d83d0ab592e0e147e98f64d827
heewon7318 <huiwon1488@gmail.com>
2022-08-11 05:16:51 +0900

    [S07P12D209-368] feature - 입양관리 미팅 오픈비두 UI

--- frontend/src/components/adopt/meeting/MeetingWait.vue
+++ frontend/src/components/adopt/meeting/MeetingWait.vue
@@ -1,17 +1,17 @@
 <template>
 	<div>
-		<div style="height: 10vh"></div>
 		<!-- 이름 -->
-		<div style="text-align: center; color: white"><h3>황희원</h3></div>
+		<div class="name"><h3>황희원</h3></div>
 		<!-- 프로필 사진 -->
-		<div style="height: 40vh; display: flex; align-content: center">
-			<div
-				style="border: 1px solid black; width: 40%; height: 40%; margin: auto"
-			></div>
+		<div class="profile-wrapper">
+			<div class="profile-image-wrapper">
+				<img
+					src="https://images.pexels.com/photos/33053/dog-young-dog-small-dog-maltese.jpg"
+					alt="profile image"
+					class="profile-image"
+				/>
+			</div>
 		</div>
-		<!-- 문구 -->
-		<div style="text-align: center; color: white">대기 중 입니다.</div>
-		<div style="height: 10vh"></div>
 	</div>
 </template>
 

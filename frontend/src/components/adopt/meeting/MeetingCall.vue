c367557a21f96ebca56a45c295e5c905c7e848d5
heewon7318 <huiwon1488@gmail.com>
2022-08-10 21:56:33 +0900

    feature - 프론트엔드에서만 미팅 구현

--- frontend/src/components/adopt/meeting/MeetingCall.vue
+++ frontend/src/components/adopt/meeting/MeetingCall.vue
@@ -31,9 +31,9 @@
 </template>
 
 <script>
-import { useStore } from 'vuex';
+// import { useStore } from 'vuex';
 import axios from 'axios';
-import api from '@/api/api';
+// import api from '@/api/api';
 // import { computed } from 'vue';
 import { OpenVidu } from 'openvidu-browser';
 import UserVideo from '@/components/adopt/meeting/UserVideo';
@@ -42,7 +42,7 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 export default {
 	components: { UserVideo },
 	setup() {
-		const store = useStore();
+		// const store = useStore();
 		let OV = undefined;
 		let session = undefined;
 		let mainStreamManager = undefined;
@@ -53,24 +53,94 @@ export default {
 		// const userSeq = computed(() => store.getters['auth/user']?.userSeq);
 		const userSeq = 1;
 
-		const getOpenViduToken = userSeq => {
-			return new Promise(resolve => {
-				axios({
-					url: api.meeting.getOpenViduToken(userSeq),
-					method: 'post',
-					headers: store.getters['auth/authHeader'],
-				})
-					.then(res => res.body.data)
-					.then(data => {
-						resolve(data.token);
-						console.log('백엔드에 토큰 요청');
-						console.log(data.token);
-					})
+		//-------------------------------------------------------------------------------------------------------------
+		const OPENVIDU_SERVER_URL = 'https://i7d209.p.ssafy.io:8442';
+		const OPENVIDU_SERVER_SECRET = 'mung209pro';
+
+		const getToken = mySessionId => {
+			return createSession(mySessionId).then(sessionId =>
+				createToken(sessionId),
+			);
+		};
+
+		// See https://docs.openvidu.io/en/stable/reference-docs/REST-API/#post-session
+		const createSession = sessionId => {
+			return new Promise((resolve, reject) => {
+				axios
+					.post(
+						`${OPENVIDU_SERVER_URL}/openvidu/api/sessions`,
+						JSON.stringify({
+							customSessionId: sessionId,
+						}),
+						{
+							auth: {
+								username: 'OPENVIDUAPP',
+								password: OPENVIDU_SERVER_SECRET,
+							},
+						},
+					)
+					.then(response => response.data)
+					.then(data => resolve(data.id))
 					.catch(error => {
-						console.log(error);
+						if (error.response.status === 409) {
+							resolve(sessionId);
+						} else {
+							console.warn(
+								`No connection to OpenVidu Server. This may be a certificate error at ${OPENVIDU_SERVER_URL}`,
+							);
+							if (
+								window.confirm(
+									`No connection to OpenVidu Server. This may be a certificate error at ${OPENVIDU_SERVER_URL}\n\nClick OK to navigate and accept it. If no certificate warning is shown, then check that your OpenVidu Server is up and running at "${OPENVIDU_SERVER_URL}"`,
+								)
+							) {
+								location.assign(`${OPENVIDU_SERVER_URL}/accept-certificate`);
+							}
+							reject(error.response);
+						}
 					});
 			});
 		};
+
+		// See https://docs.openvidu.io/en/stable/reference-docs/REST-API/#post-connection
+		const createToken = sessionId => {
+			return new Promise((resolve, reject) => {
+				axios
+					.post(
+						`${OPENVIDU_SERVER_URL}/openvidu/api/sessions/${sessionId}/connection`,
+						{},
+						{
+							auth: {
+								username: 'OPENVIDUAPP',
+								password: OPENVIDU_SERVER_SECRET,
+							},
+						},
+					)
+					.then(response => response.data)
+					.then(data => resolve(data.token))
+					.catch(error => reject(error.response));
+			});
+		};
+
+		//------------------------------------------------------------------------------------------------------------
+
+		// const getOpenViduToken = userSeq => {
+		// 	return new Promise(resolve => {
+		// 		axios({
+		// 			url: api.meeting.getOpenViduToken(userSeq),
+		// 			method: 'post',
+		// 			headers: store.getters['auth/authHeader'],
+		// 		})
+		// 			.then(res => res.body.data)
+		// 			.then(data => {
+		// 				resolve(data.token);
+		// 				console.log('백엔드에 토큰 요청');
+		// 				console.log(data.token);
+		// 			})
+		// 			.catch(error => {
+		// 				console.log(error);
+		// 			});
+		// 	});
+		// };
 
 		const joinSession = () => {
 			// --- Get an OpenVidu object ---
@@ -105,7 +175,7 @@ export default {
 			});
 
 			// 토큰 받고 처리
-			getOpenViduToken(userSeq).then(token => {
+			getToken(userSeq).then(token => {
 				console.log('getOpenViduToken 토큰 받고 처리');
 				session
 					.connect(token, { clientData: myUserName })
@@ -146,16 +216,16 @@ export default {
 		const leaveSession = () => {
 			console.log('leaveSession');
 			// session.disconnect 를 이용하여 session 연결 해제
-			if (this.session) this.session.disconnect();
+			if (session) session.disconnect();
 			// 현재 data들 모두 초기화
-			this.session = undefined;
-			this.mainStreamManager = undefined;
-			this.publisher = undefined;
-			this.subscribers = [];
-			this.OV = undefined;
+			session = undefined;
+			mainStreamManager = undefined;
+			publisher = undefined;
+			subscribers = [];
+			OV = undefined;
 
 			// eventlistener 삭제
-			window.removeEventListener('beforeunload', this.leaveSession);
+			window.removeEventListener('beforeunload', leaveSession);
 		};
 
 		const updateMainVideoStreamManager = stream => {

43d4c469ed4849394e2f36862f3203294aa37709
mhlee <mihyun0921@gmail.com>
2022-08-12 16:28:47 +0900

    [S07P12D209-286] feature - BE chatting API 구현

--- backend/src/main/java/com/d209/mungtopia/controller/ChattingController.java
+++ backend/src/main/java/com/d209/mungtopia/controller/ChattingController.java
@@ -5,12 +5,11 @@ import com.d209.mungtopia.dto.applicant.ChatLogDto;
 import com.d209.mungtopia.service.ChattingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/chat")
@@ -24,11 +23,10 @@ public class ChattingController {
         return chattingService.chattingHandler(chatLogDto);
     }
 
-    @GetMapping("/log/{page_no}/{chat_room_id}")
-    public ApiResponse chatLog(@PathVariable("page_no") int pageNo,
-                               @PathVariable("chat_room_id") Long chatRoomId,
-                               Pageable pageable) {
-        return ApiResponse.success("data", chattingService.chatLog(pageNo, chatRoomId, pageable));
+    @GetMapping("/log")
+    public ApiResponse chatLog(@RequestParam("page") int page,
+                               @RequestParam("chat_room_id") Long chatRoomId) {
+        return ApiResponse.success("data", chattingService.chatLog(page, chatRoomId));
     }
 
 }

aa061aecd27e7890d099cb88de392556b1d3383f
anwlro7958 <yeonjeong.lee00@gmail.com>
2022-08-12 16:41:10 +0900

    fix - cors error origins 추가

--- backend/src/main/java/com/d209/mungtopia/MungtopiaApplication.java
+++ backend/src/main/java/com/d209/mungtopia/MungtopiaApplication.java
@@ -11,10 +11,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 // 해당 클래스를 빈으로 등록하고, 프로퍼티 값을 할당
 // -> 이렇게 Configuration을 이용해서 지정할 수도 있고 각각의 클래스에 @Configuration을 붙여도 된다.
 @EnableConfigurationProperties({
-		AppProperties.class
-//		CorsProperties.class
+		AppProperties.class,
+		CorsProperties.class
 })
-//@CrossOrigin("http://localhost:3000")
 public class MungtopiaApplication {
 
 	public static void main(String[] args) {

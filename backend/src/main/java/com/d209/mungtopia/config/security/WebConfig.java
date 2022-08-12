628795c811483abd5bc2c663f09c596467f0ceea
anwlro7958 <anwlro7958@gmail.com>
2022-08-12 16:41:10 +0900

    fix - cors error origins 추가

--- backend/src/main/java/com/d209/mungtopia/config/security/WebConfig.java
+++ backend/src/main/java/com/d209/mungtopia/config/security/WebConfig.java
@@ -1,17 +1,17 @@
-package com.d209.mungtopia.config.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://i7d209.p.ssafy.io")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                .allowCredentials(true)
-                .allowedHeaders("*")
-                .maxAge(3000);
-    }
-}
+//package com.d209.mungtopia.config.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    public void addCorsMappings(CorsRegistry registry){
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000", "https://i7d209.p.ssafy.io")
+//                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+//                .allowCredentials(true)
+//                .allowedHeaders("*")
+//                .maxAge(3000);
+//    }
+//}

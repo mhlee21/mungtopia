e705b67a6438f2d2418dbf3265bc55e028636df1
gogwang123 <102132@naver.com>
2022-08-12 23:31:30 +0900

    [S07P12D209-404] feature-게임-마크업-매칭 결과 페이지 구현

--- frontend/src/views/game/GameMainView.vue
+++ frontend/src/views/game/GameMainView.vue
@@ -8,16 +8,43 @@
 			<!-- 게임 리스트 -->
 			<div class="game-list">
 				<div class="game-btn" @click="gameEnter(0)">
-					<img src="@/assets/images/knowledge.jpg" alt="" />
+					<img class="game-img" src="@/assets/images/knowledge.jpg" alt="" />
 					<div class="game-title">강아지식 테스트</div>
+					<div class="circle" v-if="isClear[0]">
+						<img
+							class="stamp"
+							:src="require('@/assets/images/' + 'dogPaws' + '.png')"
+							width="50"
+							height="50"
+							alt=""
+						/>
+					</div>
 				</div>
 				<div class="game-btn" @click="gameEnter(1)">
-					<img src="@/assets/images/mbti.jpg" alt="" />
+					<img class="game-img" src="@/assets/images/mbti.jpg" alt="" />
 					<div class="game-title">댕BTI</div>
+					<div class="circle" v-if="isClear[1]">
+						<img
+							class="stamp"
+							:src="require('@/assets/images/' + 'dogPaws' + '.png')"
+							width="50"
+							height="50"
+							alt=""
+						/>
+					</div>
 				</div>
 				<div class="game-btn" @click="gameEnter(2)">
-					<img src="@/assets/images/matching.jpg" alt="" />
+					<img class="game-img" src="@/assets/images/matching.jpg" alt="" />
 					<div class="game-title">나와 잘맞는 반려견은?</div>
+					<div class="circle" v-if="isClear[2]">
+						<img
+							class="stamp"
+							:src="require('@/assets/images/' + 'dogPaws' + '.png')"
+							width="50"
+							height="50"
+							alt=""
+						/>
+					</div>
 				</div>
 			</div>
 			<NavBar></NavBar>
@@ -37,6 +64,11 @@ export default {
 	},
 	setup() {
 		const store = useStore();
+		const user = store.getters['auth/user'];
+		if (user != null) {
+			store.dispatch('game/receiveClear');
+		}
+		const isClear = store.getters['game/isClear'];
 		const gameEnter = gameType => {
 			store.dispatch('game/enterGame', gameType);
 			router.push({
@@ -44,7 +76,7 @@ export default {
 				params: { gameType: gameType },
 			});
 		};
-		return { gameEnter, NavBar };
+		return { gameEnter, NavBar, isClear };
 	},
 };
 </script>
@@ -86,10 +118,14 @@ export default {
 	padding: 25px 30px 85px;
 	box-sizing: border-box;
 }
-img {
+.game-img {
 	width: 130px;
 	height: 130px;
 	border-radius: 1rem;
+}
+.stamp {
+	width: 50px;
+	height: 40px;
 }
 .title {
 	text-align: center;
@@ -110,5 +146,24 @@ img {
 .game-menu-pg {
 	display: flex;
 	justify-content: center;
+}
+
+.material-symbols-outlined {
+	font-variation-settings: 'FILL' 0, 'wght' 200, 'GRAD' 0, 'opsz' 48;
+	font-size: 50px !important;
+}
+
+.paws {
+	border-radius: 50%;
+	width: 50px !important;
+	height: 50px !important;
+}
+
+.circle {
+	margin: 0 auto;
+	width: 50px;
+	height: 45px;
+	border: 3px solid #000;
+	border-radius: 50%;
 }
 </style>

3ac5c64d8d88adfe8b8653f79436bedce19270ea
heewon7318 <huiwon1488@gmail.com>
2022-08-13 14:09:06 +0900

    [S07P12D209-410] feature - 입양보내기 상세 수정

--- frontend/src/components/adopt/main/AdoptStateComponent.vue
+++ frontend/src/components/adopt/main/AdoptStateComponent.vue
@@ -1,51 +1,41 @@
 <template>
 	<div class="adopt-state-component">
-		<!-- 입양하기 -->
-		<div v-if="isApplicant">
+		<div v-if="mainList?.length">
 			<div
-				v-for="(mainTitle, index) in applicantMainTitle"
-				:key="index"
+				v-for="(mainTitle, status) in mainTitleList[isApplicant ? 1 : 0]"
+				:key="status"
 				class="component-wrapper"
 			>
-				<div class="title-wrapper">
+				<div class="title-wrapper" v-if="applicationList[status]?.length">
 					<i class="fa-solid fa-heart title-icon"></i>
 					<h3 class="title">{{ mainTitle }}</h3>
 				</div>
-				<div
-					v-for="application in applicantMainList[index]"
-					:key="application.appicationId"
-				>
-					<ApplicantMainComponent
-						:application="application"
-					></ApplicantMainComponent>
+				<div v-if="isApplicant">
+					<div
+						v-for="application in applicationList[status]"
+						:key="application.appicationId"
+					>
+						<ApplicantMainComponent
+							:application="application"
+						></ApplicantMainComponent>
+					</div>
+				</div>
+				<div v-else>
+					<div v-for="board in applicationList[status]" :key="board.boardId">
+						<ProtectorMainComponent :board="board"></ProtectorMainComponent>
+					</div>
 				</div>
 			</div>
 		</div>
-		<!-- 입양보내기 -->
-		<div v-else>
-			<div
-				v-for="(mainTitle, index) in protectorMainTitle"
-				:key="index"
-				class="component-wrapper"
-			>
-				<div class="title-wrapper">
-					<i class="fa-solid fa-heart title-icon"></i>
-					<h3 class="title">{{ mainTitle }}</h3>
-				</div>
-				<div v-for="board in protectorMainList[index]" :key="board.boardId">
-					<ProtectorMainComponent :board="board"></ProtectorMainComponent>
-				</div>
-			</div>
-		</div>
+		<div v-else>입양 관련 기록이 존재하지 않습니다</div>
 	</div>
 </template>
 
 <script>
 import ApplicantMainComponent from '@/components/adopt/main/ApplicantMainComponent';
 import ProtectorMainComponent from '@/components/adopt/main/ProtectorMainComponent';
-import { computed, reactive } from 'vue';
+import { computed } from 'vue';
 import { useStore } from 'vuex';
-import { useRoute } from 'vue-router';
 export default {
 	components: {
 		ApplicantMainComponent,
@@ -53,45 +43,39 @@ export default {
 	},
 	setup() {
 		const store = useStore();
-		const route = useRoute();
-		const applicantMainTitle = reactive([
-			'입양 중이예요!',
-			'새로운 가족이 되었어요!',
-		]);
-		const protectorMainTitle = reactive([
-			'새로운 가족을 찾고 있어요!',
-			'새로운 가족을 찾았어요!',
-		]);
-		store.dispatch('adopt/fetchApplicantMainList', route.params.userSeq);
+		const mainTitleList = [
+			['새로운 가족을 찾고 있어요!', '새로운 가족을 찾았어요!'],
+			['입양 중이예요!', '새로운 가족이 되었어요!'],
+		];
 
 		const isApplicant = computed(() => store.getters['adopt/isApplicant']);
-		const applicantMainList = computed(() => {
-			return [
-				store.getters['adopt/applicantMainList'].filter(
-					application => application.applicationStatus < 6,
-				),
-				store.getters['adopt/applicantMainList'].filter(
-					application => application.applicationStatus === 6,
-				),
-			];
-		});
-		const protectorMainList = computed(() => {
-			return [
-				store.getters['adopt/protectorMainList'].filter(
-					application => application.adoptionStatus === 0,
-				),
-				store.getters['adopt/protectorMainList'].filter(
-					application => application.adoptionStatus === 1,
-				),
-			];
-		});
-
+		const mainList = computed(() => store.getters['adopt/mainList']);
+		const applicationList = computed(() =>
+			isApplicant.value && mainList.value
+				? [
+						mainList?.value?.filter(
+							application =>
+								application.applicationStatus < 6 ||
+								application.applicationStatus === 8,
+						),
+						mainList?.value?.filter(
+							application => application.applicationStatus === 6,
+						),
+				  ]
+				: [
+						mainList?.value?.filter(
+							application => application.adoptionStatus === false,
+						),
+						mainList?.value?.filter(
+							application => application.adoptionStatus === true,
+						),
+				  ],
+		);
 		return {
 			isApplicant,
-			applicantMainTitle,
-			protectorMainTitle,
-			applicantMainList,
-			protectorMainList,
+			applicationList,
+			mainList,
+			mainTitleList,
 		};
 	},
 };

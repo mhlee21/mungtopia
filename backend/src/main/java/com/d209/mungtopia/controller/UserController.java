5c38b6f173a51850620c91a9bf5444c6dd1a99d5
anwlro7958 <anwlro7958@gmail.com>
2022-08-16 17:16:10 +0900

    fix - user 저장 부분 수정

--- backend/src/main/java/com/d209/mungtopia/controller/UserController.java
+++ backend/src/main/java/com/d209/mungtopia/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.d209.mungtopia.controller;
 import com.d209.mungtopia.dto.user.UseInfoReq;
+import com.d209.mungtopia.dto.user.UserNicknameRes;
 import com.d209.mungtopia.entity.UserInfo;
 import com.d209.mungtopia.service.UserService;
 import com.d209.mungtopia.common.ApiResponse;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -90,4 +92,8 @@ public class UserController {
         return ApiResponse.success("data", userService.getUserBoard(userSeq));
     }
 
+    @PutMapping("/name/{userSeq}")
+    public ApiResponse putUserNickName(@PathVariable Long userSeq, @RequestBody UserNicknameRes nickname){
+        return ApiResponse.success("data", userService.putUserNickName(userSeq, nickname.getNickname()));
+    }
 }

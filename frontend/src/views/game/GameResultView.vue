d528c7db6e6b964c81e82b2afdb36fe7208e6814
황희원 <heewon7318@naver.com>
2022-08-17 02:14:38 +0900

    게임 페이지 수정

--- frontend/src/views/game/GameResultView.vue
+++ frontend/src/views/game/GameResultView.vue
@@ -2,29 +2,61 @@
 	<div class="game-result-pg">
 		<div class="game-header">반려견 매칭 결과</div>
 		<div class="game-body">
-			<div class="dog-image-wrapper"><img src="" alt="" /></div>
+			<div class="dog-image-wrapper" @click="clickDogImage">
+				<img :src="dogImg" alt="dogImg" class="dog-img" />
+			</div>
 			<div class="dog-notice">
-				{{ dogName }}몽이에 대해 더 자세히 알고 싶다면 터치해주세요!
+				{{ dogName }}(이)에 대해 더 자세히 알고 싶다면 터치해주세요!
 			</div>
 			<div class="dog-matching">
 				<div class="dog-matching-title">
-					<span class="dog-name">{{ dogName }}몽이</span>
+					<span class="dog-name">{{ dogName }}</span>
 					와(과) 매칭률
 				</div>
-				<div class="matching-percent">{{ matchingPercent }}87%</div>
+				<div class="matching-percent">{{ percent }}%</div>
 			</div>
-		</div>
-		<div class="button-wrapper">
-			<a click="menu-btn">Back to Menu</a>
-			<button @click="share" class="share-btn">Share</button>
+			<div class="button-wrapper">
+				<a class="menu-btn" @click="clickMenu">Back to Menu</a>
+				<button @click="clickShare" class="share-btn">Share</button>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+
 export default {
 	setup() {
-		return {};
+		const router = useRouter();
+		const clickMenu = () => {
+			router.push({ name: 'gameIntro' });
+		};
+		const store = useStore();
+		const matchData = computed(() => store.getters['game/matchData']);
+		const boardId = computed(() => matchData.value['boardId']);
+		const percent = computed(() => matchData.value['percent']);
+		const dogName = computed(() => matchData.value['dogName']);
+		const dogImg = computed(() => matchData.value['dogImg']);
+		const clickDogImage = () => {
+			router.push({ path: '/board/detail/' + boardId.value });
+		};
+		const clickShare = () => {
+			window.Kakao.Link.sendCustom({
+				templateId: 81265,
+			});
+		};
+		return {
+			clickMenu,
+			clickDogImage,
+			clickShare,
+			matchData,
+			percent,
+			dogName,
+			dogImg,
+		};
 	},
 };
 </script>
